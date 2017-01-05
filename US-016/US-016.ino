f2632e015731f20b97713a63a27f72668735516d
김재균 <worbs0604@gmail.com>
2017-01-05 17:21:00 +0900

    Add files via upload

---
+++ US-016/US-016.ino
@@ -0,0 +1,15 @@
+int US = A0;
+int om ; // one meter
+int tm ; // three meter
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  om = analogRead(US);
+  tm = om * 3;
+  Serial.print(om); // one meter
+  // Serial.print(tm); // three meter
+  Serial.println(" mm");
+  delay(100);
+}
